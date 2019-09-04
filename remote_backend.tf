09092e154402b03a9dda7a0912487ff193204807
Tanner Mjelde <tmjelde@costco.com>
2019-09-04 21:18:11 +0000

    Add in the tags

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "tmjelde-sandbox"
     workspaces {
       name = "hashicat-azure"
